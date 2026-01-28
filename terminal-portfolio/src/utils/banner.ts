e3d72e2f0b9e81aebdc70ea62d4802bab63277a6
Jonsson132123 <albinjonsson03@hotmail.se>
2026-01-28 15:19:11 +0100

    Uppdaterat verision och prövat git push

--- terminal-portfolio/src/utils/banner.ts
+++ terminal-portfolio/src/utils/banner.ts
@@ -6,5 +6,5 @@ export const banner = `
   \\/_/\\/_/   \\/_____/   \\/_____/   \\/_/   \\/_/ \\/_/   \\/_____/   \\/_____/ 
                                                                           
 
-       Welcome to AlbinOS v0.2.1
+       Welcome to AlbinOS v0.2.5
 `;
