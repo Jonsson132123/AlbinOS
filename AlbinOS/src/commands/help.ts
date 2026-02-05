d6b7d6372407ce8cf87567bdf759318fe926db46
Jonsson132123 <albinjonsson03@hotmail.se>
2026-02-05 21:14:32 +0100

    fix

--- AlbinOS/src/commands/help.ts
+++ AlbinOS/src/commands/help.ts
@@ -14,6 +14,8 @@ pwd        - Print working directory
 cat        - Concatenate files to standard output
 ls         - List directory contents
 cd         - Change directory
+
+
         `;
         return {
             action: 'print',
